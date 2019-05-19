ae8727fa876cb5377fd554052e7ba9b658c2612f
thomascountz <thomascountz@gmail.com>
2019-05-19 18:02:07 -0400

    limit bad calls to api

--- app/controllers/bobby_controller.rb
+++ app/controllers/bobby_controller.rb
@@ -4,9 +4,12 @@ class BobbyController < ApplicationController
   def index; end
 
   def search
+    stop_code = params[:stop_id]
+    return redirect_to :root unless stop_code.length == 6
+
     http_client = HTTPClient.new
     mta_bus_info_client = MTABusInfoClient.new(http_client: http_client)
-    result = mta_bus_info_client.fetch_stop_monitoring(stop_code: params[:stop_id])
+    result = mta_bus_info_client.fetch_stop_monitoring(stop_code: stop_code)
     stop_monitor = StopMonitorProcessor.process(json: result)
     return redirect_to :root unless stop_monitor.error_description.nil?
 
